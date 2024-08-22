de13235562928ffaab926b7d29ed8790ccd43867
nstylo <niklas@stylianou.info>
2024-08-22 11:59:52 +0200

    Fix coloring for comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -138,8 +138,35 @@ return {
   {
     "folke/todo-comments.nvim",
     lazy = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      require("todo-comments").setup()
+      require("todo-comments").setup {
+        signs = true, -- show icons in the signs column
+        sign_priority = 1, -- sign priority
+        keywords = {
+          FIX = {
+            icon = " ", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = " ", color = "todo" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+          TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        colors = {
+          error = { "@character" },
+          warning = { "@boolean" },
+          todo = { "@attribute" },
+          info = { "@function" },
+          hint = { "@constructor" },
+          default = { "@character" },
+          test = { "@character" },
+        },
+      }
     end,
   },
 }
